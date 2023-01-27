ecfc699d1342de7a57a1a1bca676a2bf2a624b47
Fezzer <chris.bowles@thescreamingsheep.net>
2023-01-27 17:47:18 +0000

    use return from geocoder endpoint to call the forecast endpoint

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,6 @@
 const API_KEY = "e2fbfb8cefa7140e915e1270b734b104";
 const GEOCODER_ROOT_URL = "http://api.openweathermap.org/geo/1.0/direct";
+const FORECAST_ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast"
 
 // Makes a call to the geocoder endpoint and forwards the result to the callback.
 function getGeocode(search, callback) {
@@ -7,23 +8,46 @@ function getGeocode(search, callback) {
 
   fetch(queryUrl)
     .then(response => response.json())
-    .then(callback)
-    .catch(x => console.log(x));
+    .then(data => {
+      const element = data[0];
+      console.log(element);
+      console.log(`lat: ${element.lat}; log: ${element.lon}`);
+      callback(data[0].lat, data[0].lon, displayObject);
+    })
+    .catch(console.log);
 }
 
-// Displays a geocode.
-function displayGeocode(geocode) {
-  const pre = document.createElement("pre");
-  pre.textContent = JSON.stringify(geocode, null, 2);
-  document.getElementById("forecast").append(pre);
+// Makes a call to the 5 day weather forecast endpoint and forwards the result to the callback.
+function getWeatherForecast(lat, lon, callback) {
+  const queryUrl = `${FORECAST_ROOT_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+  fetch(queryUrl)
+    .then(response => response.json())
+    .then(callback)
+    .catch(console.log);
 }
+
+// Displays a object at the bottom of the page.
+function displayObject(object) {
+  let pre = document.getElementById("object-display");
+
+  if (!pre) {
+    pre = document.createElement("pre");
+    pre.setAttribute("id", "object-display");
+    document.body.append(pre);
+  }
+
+  pre.textContent = JSON.stringify(object, null, 2);
+}
+
+// Displays the weather forecast.
 
 // Handler for the search button click.
 function searchButtonClick(event) {
   event.preventDefault();
 
   const search = document.getElementById("search-input").value;
-  getGeocode(search, displayGeocode);
+  getGeocode(search, getWeatherForecast);
 }
 
 document.getElementById("search-button").addEventListener("click", searchButtonClick);
